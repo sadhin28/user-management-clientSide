5437f17651c8d6075535cc6e6fe964366fbab512
sadhin28 <taosifsadhin113@gmail.com>
2025-06-21 01:43:27 +0600

    update value

--- src/component/Update.jsx
+++ src/component/Update.jsx
@@ -8,11 +8,11 @@ const Update = () => {
     }
     return (
         <div>
-             <form onSubmit={handelSubmit} className="grid py-20 border-2  card-body max-w-6/12 md:w-3/12  mx-auto items-center  mt-20 bg-gray-600 p-10 shadow-2xs gap-2 rounded-2xl">
-                <input type="text" placeholder="Your name" name="name" className="border-white border-2 h-10 rounded-xl w-full p-2"/>
-                <input type="email" placeholder="youremail@gmail.com" name="email" id="" className="border-white w-full border-2 h-10 rounded-xl p-2" />
+             <form onSubmit={handelSubmit} className="grid py-20 text-white border-2  card-body max-w-6/12 md:w-3/12  mx-auto items-center  mt-20 bg-gray-600 p-10 shadow-2xs gap-2 rounded-2xl">
+                <input defaultValue={data?.name} type="text" placeholder="Your name" name="name" className="border-white border-2 h-10 rounded-xl w-full p-2"/>
+                <input defaultValue={data?.email} type="email" placeholder="youremail@gmail.com" name="email" id="" className="border-white w-full border-2 h-10 rounded-xl p-2" />
                 <br />
-                <input type="submit" value='Add User' name="Submit" className="border-black h-10 hover:bg-blue-700 hover:text-white hover:font-bold bg-purple-500 rounded-xl p-2" />
+                <input type="submit" value='Update User' name="Submit" className="border-black h-10 hover:bg-blue-700 font-bold hover:text-white hover:font-bold bg-purple-500 rounded-xl p-2" />
             </form>
         </div>
     );
